5419784f4041fda6dd58e17f5f5e3f5cd69625d6
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-02-26 22:13:18 +0900

    youtubeの接続処理

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"main"
+	"fmt"
+	"log"
+	"net/http"
+
+	"google.golang.org/api/googleapi/transport"
+	youtube "google.golang.org/api/youtube/v3"
 )
+
+const developerKey = "aaa"
 
 func main() {
 	fmt.Println("Hello World")
+	client := &http.Client{
+		Transport: &transport.APIKey{Key: developerKey},
+	}
+
+	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Error creating new YouTube client: %v", err)
+	}
+	call := service.Channels.List("contentDetails").Mine(true)
+
+	response, err := call.Do()
+	if err != nil {
+		// The channels.list method call returned an error.
+		log.Fatalf("Error making API call to list channels: %v", err.Error())
+	}
+
+	log.Fatal(response)
 }
