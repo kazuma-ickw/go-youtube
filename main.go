3060b7a05aa9a7e90daa0118cd9de62634ed8527
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-02-27 22:25:49 +0900

    youtube APIのサンプルを試す

--- main.go
+++ main.go
@@ -1,27 +1,49 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/BurntSushi/toml"
 
 	"google.golang.org/api/googleapi/transport"
 	youtube "google.golang.org/api/youtube/v3"
 )
 
-const developerKey = "aaa"
+// Config is a root-level configration object
+type Config struct {
+	API APIConfig
+}
+
+// APIConfig is a configration object for youtube api
+type APIConfig struct {
+	Key string `toml:"key"`
+}
+
+var (
+	query      = flag.String("query", "Google", "Search term")
+	maxResults = flag.Int64("max-results", 25, "Max YouTube results")
+)
 
 func main() {
+
+	var config Config
+	_, err := toml.DecodeFile("config.toml", &config)
+
 	fmt.Println("Hello World")
 	client := &http.Client{
-		Transport: &transport.APIKey{Key: developerKey},
+		Transport: &transport.APIKey{Key: config.API.Key},
 	}
 
 	service, err := youtube.New(client)
 	if err != nil {
 		log.Fatalf("Error creating new YouTube client: %v", err)
 	}
-	call := service.Channels.List("contentDetails").Mine(true)
+	call := service.Search.List("id,snippet").
+		Q(*query).
+		MaxResults(*maxResults)
 
 	response, err := call.Do()
 	if err != nil {
@@ -29,5 +51,34 @@ func main() {
 		log.Fatalf("Error making API call to list channels: %v", err.Error())
 	}
 
+	// Group video, channel, and playlist results in separate lists.
+	videos := make(map[string]string)
+	channels := make(map[string]string)
+	playlists := make(map[string]string)
+
+	// Iterate through each item and add it to the correct list.
+	for _, item := range response.Items {
+		switch item.Id.Kind {
+		case "youtube#video":
+			videos[item.Id.VideoId] = item.Snippet.Title
+		case "youtube#channel":
+			channels[item.Id.ChannelId] = item.Snippet.Title
+		case "youtube#playlist":
+			playlists[item.Id.PlaylistId] = item.Snippet.Title
+		}
+	}
+
+	printIDs("Videos", videos)
+	printIDs("Channels", channels)
+	printIDs("Playlists", playlists)
+
 	log.Fatal(response)
+}
+
+func printIDs(sectionName string, matches map[string]string) {
+	fmt.Printf("%v:\n", sectionName)
+	for id, title := range matches {
+		fmt.Printf("[%v] %v\n", id, title)
+	}
+	fmt.Printf("\n\n")
 }
